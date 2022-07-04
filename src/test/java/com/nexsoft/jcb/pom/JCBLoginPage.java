3e77a84e43e8532de25fc882a6e30b6db160ac5b
Antoni Kurniawan <antoni.nexsoft@gmail.com>
2022-07-04 10:29:22 +0700

    first commit

--- src/test/java/com/nexsoft/jcb/pom/JCBLoginPage.java
+++ src/test/java/com/nexsoft/jcb/pom/JCBLoginPage.java
@@ -1,5 +1,57 @@
 package com.nexsoft.jcb.pom;
 
-public class JCBLoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class JCBLoginPage {
+	WebDriver driver;
+	
+	@FindBy(xpath = "//input[@placeholder='User']")
+	private WebElement fieldUsername;
+	
+	@FindBy(xpath = "//input[@placeholder='Password']")
+	private WebElement fieldPassword;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	private WebElement btnLogin;
+	
+	@FindBy(xpath = "//small[normalize-space()='User / Password tidak sesuai.']")
+	private WebElement pesanErrorInvalidUserPassword;
+	
+//	@FindBy(xpath = "")
+//	private WebElement promptAlertUserEmpty;
+//	
+//	@FindBy(xpath = "")
+//	private WebElement promptAlertPasswordEmpty;
+	
+	
+	public JCBLoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public JCBLoginPage inputFieldUsername(String username) {
+		fieldUsername.clear();
+		fieldUsername.sendKeys(username);
+		return PageFactory.initElements(driver, JCBLoginPage.class);
+		
+	}
+	
+	public JCBLoginPage inputFieldPassword(String password) {
+		fieldPassword.clear();
+		fieldPassword.sendKeys(password);
+		return PageFactory.initElements(driver, JCBLoginPage.class);
+	}
+	
+	public String getPromptAlertTextFieldEmpty() {
+		return driver.switchTo().alert().getText();
+	}
+	
+	public String getErrMsgInvalidUsernamePassword() {
+		return pesanErrorInvalidUserPassword.getText();
+	}
+	
+	
+	
 }
--- src/test/java/com/nexsoft/jcb/test/TestModulLogin.java
+++ src/test/java/com/nexsoft/jcb/test/TestModulLogin.java
@@ -3,7 +3,65 @@ package com.nexsoft.jcb.test;
 import org.testng.annotations.Test;
 
 public class TestModulLogin {
-  @Test
-  public void f() {
-  }
+	@Test(priority = 0)
+	public void input_username_tanpa_password_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 1)
+	public void input_password_tanpa_username_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 2)
+	public void input_username_invalid_dan_password_valid_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 3)
+	public void input_username_valid_dan_password_invalid_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 4)
+	public void input_username_panjang_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 5)
+	public void input_password_panjang_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 6)
+	public void input_username_valid_dengan_huruf_besar_kecil_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 7)
+	public void input_password_valid_dengan_huruf_besar_kecil_dan_tekan_login(){
+
+	}
+	
+	@Test(priority = 8)
+	public void username_dan_password_kosong(){
+
+	}
+	
+	@Test(priority = 9)
+	public void input_username_dan_password_valid_sebagai_admin(){
+
+	}
+	
+	@Test(priority = 10)
+	public void input_username_dan_password_valid_sebagai_surveyor(){
+
+	}
+	
+	@Test(priority = 11)
+	public void tekan_tombol_logout_setelah_berhasil_login(){
+
+	}
+
+
 }
